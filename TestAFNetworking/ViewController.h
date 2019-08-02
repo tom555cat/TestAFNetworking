efc71ac62c29b2a89af7ebde8aa849794c358294
Tong Leiming <tongleiming@daling.com>
2019-08-02 09:44:52 +0800

    Initial Commit

---
+++ TestAFNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAFNetworking
+//
+//  Created by tongleiming on 2019/8/2.
+//  Copyright © 2019 tongleiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAFNetworking/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TestAFNetworking
+//
+//  Created by tongleiming on 2019/8/2.
+//  Copyright © 2019 tongleiming. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
