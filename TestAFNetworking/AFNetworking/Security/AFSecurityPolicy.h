5cde5bd34613bbdc9c846b9c36356764daf9340e
BJTU-HT <bjtu-ht@163.com>
2019-08-06 00:24:26 +0800

    添加注释

--- TestAFNetworking/AFNetworking/Security/AFSecurityPolicy.h
+++ TestAFNetworking/AFNetworking/Security/AFSecurityPolicy.h
@@ -35,7 +35,42 @@ NS_ASSUME_NONNULL_BEGIN
 #warning 看这个变量在判断流程中处于哪一环节
 @property (nonatomic, assign) BOOL allowInvalidCertificates;
 
+// 是否要验证证书中的CN字段，默认是YES
 @property (nonatomic, assign) BOOL validatesDomainName;
+
+///-----------------------------------------
+/// @name 从Bundle中获取证书
+///-----------------------------------------
+
+// 当时用AFNetworking时，你必须使用这个方法去获取证书，来指定自己的安全策略
++ (NSSet <NSData *> *)certificatesInBundle:(NSBundle *)bundle;
+
+// 返回共享的默认安全策略，不允许invalid证书，需要验证CN，不验证预埋证书或者预埋证书的公钥
++ (instancetype)defaultPolicy;
+
+///---------------------
+/// @name 初始化
+///---------------------
+
+// 创建并返回一个pinningMode的安全策略
++ (instancetype)policyWithPinningMode:(AFSSLPinningMode)pinningMode;
+
+// 创建并返回一个安全策略，模式为pinningMode，并且指定了预埋证书数据
++ (instancetype)policyWithPinningMode:(AFSSLPinningMode)pinningMode withPinnedCertificates:(NSSet <NSData *> *)pinnedCertificates;
+
+/**
+ Whether or not the specified server trust should be accepted, based on the security policy.
+ 
+ This method should be used when responding to an authentication challenge from a server.
+ 
+ @param serverTrust The X.509 certificate trust of the server.
+ @param domain The domain of serverTrust. If `nil`, the domain will not be validated.
+ 
+ @return Whether or not to trust the server.
+ */
+//
+- (BOOL)evaluateServerTrust:(SecTrustRef)serverTrust
+                  forDomain:(nullable NSString *)domain;
 
 @end
 
--- TestAFNetworking/AFNetworking/Security/AFSecurityPolicy.m
+++ TestAFNetworking/AFNetworking/Security/AFSecurityPolicy.m
@@ -8,6 +8,79 @@
 
 #import "AFSecurityPolicy.h"
 
+@interface AFSecurityPolicy ()
+
+@property (readwrite, nonatomic, assign) AFSSLPinningMode SSLPinningMode;
+// 预埋证书的公钥集合
+@property (readwrite, nonatomic, strong) NSSet *pinnedPublicKeys;
+
+@end
+
 @implementation AFSecurityPolicy
+
++ (NSSet *)certificatesInBundle:(NSBundle *)bundle {
+    NSArray *paths = [bundle pathsForResourcesOfType:@"cer" inDirectory:@"."];
+    
+    NSMutableSet *certificates = [NSMutableSet setWithCapacity:[paths count]];
+    for (NSString *path in paths) {
+        NSData *certificateData = [NSData dataWithContentsOfFile:path];
+        [certificates addObject:certificateData];
+    }
+    
+    return [NSSet setWithSet:certificates];
+}
+
++ (instancetype)defaultPolicy {
+    AFSecurityPolicy *securityPolicy = [[self alloc] init];
+    securityPolicy.SSLPinningMode = AFSSLPinningModeNone;
+    
+    return securityPolicy;
+}
+
++ (instancetype)policyWithPinningMode:(AFSSLPinningMode)pinningMode {
+    return [self policyWithPinningMode:pinningMode withPinnedCertificates:[self defaultPinnedCertificates]];
+}
+
++ (instancetype)policyWithPinningMode:(AFSSLPinningMode)pinningMode withPinnedCertificates:(NSSet<NSData *> *)pinnedCertificates {
+    AFSecurityPolicy *securityPolicy = [[self alloc] init];
+    securityPolicy.SSLPinningMode = pinningMode;
+    
+    [securityPolicy setPinnedCertificates:pinnedCertificates];
+    
+    return securityPolicy;
+}
+
+// 设置证书数据
+- (void)setPinnedCertificates:(NSSet<NSData *> *)pinnedCertificates {
+    _pinnedCertificates = pinnedCertificates;
+    
+    if (self.pinnedCertificates) {
+        NSMutableSet *mutablePinnedPublicKeys = [NSMutableSet setWithCapacity:[self.pinnedCertificates count]];
+        for (NSData *certificate in self.pinnedCertificates) {
+            // 从证书中提取出公钥
+            id publickLey = AFPublicKeyForCertificate(certificate);
+            if (!publickLey) {
+                continue;
+            }
+            [mutablePinnedPublicKeys addObject:publickLey];
+        }
+        self.pinnedCertificates = [NSSet setWithSet:mutablePinnedPublicKeys];
+    } else {
+        self.pinnedPublicKeys = nil;
+    }
+}
+
+// 获取默认的证书
++ (NSSet *)defaultPinnedCertificates {
+    static NSSet *_defaultPinnedCertificates = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+        _defaultPinnedCertificates = [self certificatesInBundle:bundle];
+    });
+    
+    return _defaultPinnedCertificates;
+}
+
 
 @end
