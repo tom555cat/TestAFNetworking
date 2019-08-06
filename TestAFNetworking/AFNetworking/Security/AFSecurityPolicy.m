d143f76f2a6bfe32efbea64fcfa5a8a7a5962ada
BJTU-HT <bjtu-ht@163.com>
2019-08-07 00:38:22 +0800

    添加注释

--- TestAFNetworking/AFNetworking/Security/AFSecurityPolicy.m
+++ TestAFNetworking/AFNetworking/Security/AFSecurityPolicy.m
@@ -8,6 +8,82 @@
 
 #import "AFSecurityPolicy.h"
 
+static BOOL AFServerTrustIsValid(SecTrustRef serverTrust) {
+    BOOL isValid = NO;
+    SecTrustResultType result;
+    // SecTrustEvaluate: 同步地校验一个trust，是同步，文档建议放在非主线程中
+#warning __Require_noErr_Quiet是干什么的？对同步异步有帮助吗？
+    __Require_noErr_Quiet(SecTrustEvaluate(serverTrust, &result), _out);
+    
+    isValid = (result == kSecTrustResultUnspecified || result == kSecTrustResultProceed);
+_out:
+    return isValid;
+}
+
+static NSArray * AFCertificateTrustChainForServerTrust(SecTrustRef serverTrust) {
+    // SecTrustGetCertificateCount: 返回已经对serverTrust校验过的证书链的数目
+    CFIndex certificateCount = SecTrustGetCertificateCount(serverTrust);
+    NSMutableArray *trustChain = [NSMutableArray arrayWithCapacity:(NSUInteger)certificateCount];
+    
+    for (CFIndex i = 0; i < certificateCount; i++) {
+        // 返回trust链上的第i个证书
+        SecCertificateRef certificate = SecTrustGetCertificateAtIndex(serverTrust, i);
+        [trustChain addObject:(__bridge_transfer NSData *)SecCertificateCopyData(certificate)];
+    }
+    
+    return [NSArray arrayWithArray:trustChain];
+}
+
+static NSArray * AFPublicKeyTrustChainForServerTrust(SecTrustRef serverTrust) {
+    // SecPolicyRef: certificate trust policy
+    SecPolicyRef policy = SecPolicyCreateBasicX509();
+    // SecTrustGetCertificateCount: 获取trust校验过的证书链上的证书个数
+    CFIndex certificateCount = SecTrustGetCertificateCount(serverTrust);
+    NSMutableArray *trustChain = [NSMutableArray arrayWithCapacity:(NSUInteger)certificateCount];
+    for (CFIndex i = 0; i < certificateCount; i++) {
+        // 返回serverTrust校验过的第i个证书
+        SecCertificateRef certificate = SecTrustGetCertificateAtIndex(serverTrust, i);
+        
+        SecCertificateRef someCertificates[] = {certificate};
+        CFArrayRef certificates = CFArrayCreate(NULL, (const void **)someCertificates, 1, NULL);
+        
+        SecTrustRef trust;
+        // SecTrustCreateWithCertificates: 根据证书certificates和policy创建一个trust
+        __Require_noErr_Quiet(SecTrustCreateWithCertificates(certificates, policy, &trust), _out);
+        
+        SecTrustResultType result;
+        // 同步地校验这个trust
+        __Require_noErr_Quiet(SecTrustEvaluate(trust, &result), _out);
+        
+        // SecTrustCopyPublicKey：通过校验之后，返回trust的公钥
+        [trustChain addObject:(__bridge_transfer id)SecTrustCopyPublicKey(trust)];
+        
+    _out:
+        if (trust) {
+            CFRelease(trust);
+        }
+        
+        if (certificates) {
+            CFRelease(certificates);
+        }
+        
+        continue;
+    }
+    CFRelease(policy);
+    
+    return [NSArray arrayWithArray:trustChain];
+}
+
+static BOOL AFSecKeyIsEqualToKey(SecKeyRef key1, SecKeyRef key2) {
+#warning NSOjbect的isEqual:方法判断的是什么？
+#if TARGET_OS_IOS || TARGET_OS_WATCH || TARGET_OS_TV
+    return [(__bridge id)key1 isEqual:(__bridge id)key2];
+#else
+    return [AFSecKeyGetData(key1) isEqual:AFSecKeyGetData(key2)];
+#endif
+}
+
+
 @interface AFSecurityPolicy ()
 
 @property (readwrite, nonatomic, assign) AFSSLPinningMode SSLPinningMode;
@@ -80,6 +156,84 @@
     });
     
     return _defaultPinnedCertificates;
+}
+
+#pragma mark -
+
+- (BOOL)evaluateServerTrust:(SecTrustRef)serverTrust forDomain:(NSString *)domain {
+    if (domain && self.allowInvalidCertificates && self.validatesDomainName &&
+        (self.SSLPinningMode == AFSSLPinningModeNone || [self.pinnedCertificates count] == 0)) {
+        return NO;
+    }
+    
+    NSMutableArray *policies = [NSMutableArray array];
+    if (self.validatesDomainName) {
+        // 如果要校验域名
+        [policies addObject:(__bridge_transfer id)SecPolicyCreateSSL(true, (__bridge CFStringRef)domain)];
+    } else {
+        [policies addObject:(__bridge_transfer id)SecPolicyCreateBasicX509()];
+    }
+    
+    // 设置要校验的trust的policy
+#warning 注意CFArrayRef和NSArray之间的转换
+    SecTrustSetPolicies(serverTrust, (__bridge CFArrayRef)policies);
+    
+    if (self.SSLPinningMode == AFSSLPinningModeNone) {
+        // 如果是没有使用预埋证书校验，那么直接走系统的trust校验。
+        return self.allowInvalidCertificates || AFServerTrustIsValid(serverTrust);
+    } else if (!AFServerTrustIsValid(serverTrust) && !self.allowInvalidCertificates) {
+        // 如果使用预埋证书校验(公钥校验或者全部交验)，但是系统校验没有通过，那就直接GG了
+        return NO;
+    }
+    
+    switch (self.SSLPinningMode) {
+        // 直接在case后边就根上了大括号，就可以在大括号中定义局部变量
+        case AFSSLPinningModeCertificate: {
+            NSMutableArray *pinnedCertificates = [NSMutableArray array];
+            for (NSData *certificateData in self.pinnedCertificates) {
+                // SecCertificateCreateWithData:根据CFData创建一个证书
+                [pinnedCertificates addObject:(__bridge_transfer id)SecCertificateCreateWithData(NULL, (__bridge CFDataRef)certificateData)];
+            }
+            // 设置一个trust的证书锚点，之后校验证书就使用这些锚点证书
+            SecTrustSetAnchorCertificates(serverTrust, (__bridge CFArrayRef)pinnedCertificates);
+            
+            if (!AFServerTrustIsValid(serverTrust)) {
+                return NO;
+            }
+            
+            // 获取通过校验的证书链，应该在最后位置就是预埋证书(如果它不是根证书)
+            NSArray *serverCertificates = AFCertificateTrustChainForServerTrust(serverTrust);
+            
+            for (NSData *trustChainCertificate in [serverCertificates reverseObjectEnumerator]) {
+                // 如果通过校验的证书在我们的证书中，那么表示通过了校验
+                if ([self.pinnedCertificates containsObject:trustChainCertificate]) {
+                    return YES;
+                }
+            }
+            
+            // 通过校验的证书中不包含我们的预埋证书，那么预埋证书校验失败
+            return NO;
+        }
+            
+        case AFSSLPinningModePublicKey: {
+            NSUInteger trustedPublicKeyCount = 0;
+            // 获取校验过的证书的公钥
+            NSArray *publicKeys = AFPublicKeyTrustChainForServerTrust(serverTrust);
+            
+            for (id trustChainPublicKey in publicKeys) {
+                for (id pinnedPublicKey in self.pinnedPublicKeys) {
+                    if (AFSecKeyIsEqualToKey((__bridge SecKeyRef)trustChainPublicKey, (__bridge SecKeyRef)pinnedPublicKey)) {
+                        // 如果公钥证书相等，则+1
+                        trustedPublicKeyCount += 1;
+                    }
+                }
+            }
+            return trustedPublicKeyCount > 0;
+        }
+            
+        default:
+            return NO;
+    }
 }
 
 
