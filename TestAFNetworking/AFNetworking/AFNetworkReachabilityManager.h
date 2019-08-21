cdc61249e6a2fdff8b7375c18d477191ffbb2e26
BJTU-HT <bjtu-ht@163.com>
2019-08-22 00:16:25 +0800

    增加注释

---
+++ TestAFNetworking/AFNetworking/AFNetworkReachabilityManager.h
@@ -0,0 +1,13 @@
+//
+//  AFNetworkReachabilityManager.h
+//  TestAFNetworking
+//
+//  Created by tom555cat on 2019/8/22.
+//  Copyright © 2019年 tongleiming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFNetworkReachabilityManager : NSObject
+
+@end
---
+++ TestAFNetworking/AFNetworking/AFNetworkReachabilityManager.m
@@ -0,0 +1,13 @@
+//
+//  AFNetworkReachabilityManager.m
+//  TestAFNetworking
+//
+//  Created by tom555cat on 2019/8/22.
+//  Copyright © 2019年 tongleiming. All rights reserved.
+//
+
+#import "AFNetworkReachabilityManager.h"
+
+@implementation AFNetworkReachabilityManager
+
+@end
